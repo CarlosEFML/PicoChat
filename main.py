fe60dbf2c41ce0516f45c8c341e5f58f677ce7de
CarlosEFML <carlosefml@gmail.com>
2024-11-10 12:15:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import asyncio
+from websockets.asyncio.server import serve
+
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with serve(echo, "localhost", 443) as server:
+        await server.serve_forever()
+
+print("oi1")
+if __name__ == "__main__":
+    print("oi2")
+    asyncio.run(main())
+
 def app(environ, start_response):
     """Simplest possible application object"""
     data = b'Hello, World!\n'
